7fab7fc54af593c98eba975b2a71b1b20c2fd2fb
Benjamin <newuser@News-MBP-2.attlocal.net>
2021-01-10 00:21:00 -0800

    added commercejs functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import Products from './components/Products/Products';
+import { Products, Navbar } from './components';
+import { commerce } from './lib/commerce';
 
 function App() {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    console.log(products); 
+
     return (
         <div className="App">
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+// import { ShoppingCart } from '@material-ui/icons';
+
+function Navbar() {
+    return (
+        <div>
+
+            
+        </div>
+    )
+}
+
+export default Navbar;
